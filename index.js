205ffed8234df700af8e139f6f2695b2f58cfae8
khaled-11 <55663179+khaled-11@users.noreply.github.com>
2020-04-29 23:04:39 -0400

    Little work

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+// Include the required libraries for
 const sqlite3 = require('sqlite3').verbose();
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 var md5 = require('md5');
-var fs = require('fs');
+//var fs = require('fs');
 var Request = require("request");
 //var CryptoJS = require("crypto-js");
-const request = require('request');
 var app = express();
 app.use(session({
 	secret: 'secret',
@@ -18,20 +18,54 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 require('dotenv').config()
 
+// Setting the View and resources Folder. Choosing the EJS module. //
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get('/login', function(request, response) {
-    if (!request.session.loggedin) {
-        response.render("login");
-    } else {
-		response.send('You are already logged in!');
-	}
-	response.end();
-});
 
-
+nodeDB.run(`CREATE TABLE IF NOT EXISTS data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    crash_date ,
+    crash_time,
+    borough,
+    zip_code,
+    latitude,
+    longitude,
+    location,
+    street_name,
+    off_street_name,
+    cross_street_name,
+    number_of_persons_injured,
+    number_of_persons_killed,
+    number_of_pedestrians_injured,
+    number_of_pedestrians_killed,
+    number_of_cyclist_injured,
+    number_of_cyclist_killed,
+    number_of_motorist_injured,
+    number_of_motorist_killed,
+    contributing_factor_vehicle_1,
+    contributing_factor_vehicle_2,
+    contributing_factor_vehicle_3,
+    contributing_factor_vehicle_4,
+    contributing_factor_vehicle_5,
+    collision_id,
+    vehicle_type_code1,
+    vehicle_type_code2,
+    vehicle_type_code_3,
+    vehicle_type_code_4,
+    vehicle_type_code_5
+    )`,
+    (err) => {
+        if (err) {
+            console.log("Database Exist");
+        }else{
+            userN = process.env.USER
+            pass = md5(process.env.PASS);
+            console.log(pass);
+            nodeDB.run("INSERT INTO user (userName, userPassword, userEmail) VALUES ('"+userN+"', '"+pass+"', 'test@test.com')")
+        }
+    });  
 
 
 Request.get({
@@ -49,41 +83,7 @@ Request.get({
 
 
 
-app.get('/register', function(request, response) {
-    if (!request.session.loggedin) {
-        response.render("register");
-    } else {
-		response.send('You are already logged in!');
-	}
-	response.end();
-});
-
-app.get('/logout', function(request, response) {
-    if (request.session.loggedin) {
-      request.session.destroy()
-      response.clearCookie('connect.sid')
-      response.render("logout");
-    } else {
-	  response.send('You are not logged in!');
-    }
-    response.end();
-  });
-
-app.get('/', function(request, response) {
-	response.render("index");
-});
-
-app.get("/books", (req, res) => {
-    const sql = "SELECT * FROM user"
-    nodeDB.all(sql, [], (err, rows) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      res.render("books", { model: rows });
-    });
-  });
-
-//Creating a database and table
+// Intialize a local database and create the required tables. Inserting initial Demo user to the user table. //
 file_path = "./nodeDB.sqlite"
 let nodeDB = new sqlite3.Database(file_path, (err) => { 
     if (err) { 
@@ -97,7 +97,6 @@ let nodeDB = new sqlite3.Database(file_path, (err) => {
 			userEmail text
             )`,
             //CONSTRAINT name_unique UNIQUE (userName),
-            //CONSTRAINT email_unique UNIQUE (userEmail)
             (err) => {
                 if (err) {
                     console.log("Database Exist");
@@ -111,6 +110,7 @@ let nodeDB = new sqlite3.Database(file_path, (err) => {
     } 
   });
 
+  // Post request for web registeration. It will add the user name, password and email to the database //
   app.post('/register', function(request, res) {
 	var username = request.body.username;
     var email = request.body.email;
@@ -130,12 +130,11 @@ let nodeDB = new sqlite3.Database(file_path, (err) => {
 	}
 });
 
-
+// Post request for web login. If the user name and password found in the database, the session will established with the user name //
 app.post('/auth', function(request, response) {
 	var username = request.body.username;
-	var password = md5(request.body.password);
-
-	
+    var password = md5(request.body.password);
+    	
 	if (username && password) {
 		nodeDB.all("SELECT * FROM user WHERE userName = '"+username+"' AND userPassword = '"+password+"'", function(error, results, fields) {
             if (results.length > 0) {
@@ -153,10 +152,7 @@ app.post('/auth', function(request, response) {
 	}
 });
 
-
-
-
-
+// Sending Data page and restrict to users in session //
 app.get('/data', function(request, response) {
 	if (request.session.loggedin) {
         response.render("data");
@@ -166,4 +162,53 @@ app.get('/data', function(request, response) {
 	response.end();
 });
 
+
+app.get("/books", (req, res) => {
+    const sql = "SELECT * FROM user"
+    nodeDB.all(sql, [], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.render("books", { model: rows });
+    });
+  });
+  
+// Sending Login Page if there is no active session //
+app.get('/login', function(request, response) {
+    if (!request.session.loggedin) {
+        response.render("login");
+    } else {
+		response.send('You are already logged in!');
+	}
+	response.end();
+});
+
+// Sending Register Page if there is no active session //
+app.get('/register', function(request, response) {
+    if (!request.session.loggedin) {
+        response.render("register");
+    } else {
+		response.send('You are already logged in!');
+	}
+	response.end();
+});
+
+// Sending Register Page if the user is in active session //
+app.get('/logout', function(request, response) {
+    if (request.session.loggedin) {
+      request.session.destroy()
+      response.clearCookie('connect.sid')
+      response.render("logout");
+    } else {
+	  response.send('You are not logged in!');
+    }
+    response.end();
+  });
+
+// Sending the main index Page //  
+app.get('/', function(request, response) {
+	response.render("index");
+});
+
+// Starting the App //
 app.listen(process.env.PORT || 3370, () => console.log('webhook is listening'));
